cdbd824e23259f86e288f14c6a4631fc38657e33
Kallan <Kallan92@gmail.com>
2025-03-28 10:35:00 -0600

    Connected to mongodb. Added models/user file. Created model for user.

---
+++ app/api/lib/mongodb.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGDB_URI) {
+    throw new Error("MongoDB URI is missing.")
+}
+
+let cached = global.mongoose || { conn: null, promise: null}
+
+export async function connectToDatabase() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }).then((mongoose) => {
+            
+            console.log("MongoDB Connected!");
+            return mongoose
+        
+        });
+    }
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
